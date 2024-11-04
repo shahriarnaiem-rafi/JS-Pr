cc769febd55e161eb7966308b55b5beabb81b8d2
shahriarnaiem-rafi <shahriarnaiemrafi33@gmail.com>
2024-11-04 09:26:16 +0600

    javascript core MMM

---
+++ data-type-variable/main.js
@@ -0,0 +1,36 @@
+document.write("variable");
+document.write("<br>");
+var a=3;
+var a=4;
+var a=5;
+document.write(a);
+document.write("<br>");
+document.write("const");
+document.write("<br>");
+const b=6;
+document.write(b);
+
+document.write("<br>");
+document.write("Let");
+
+document.write("<br>");
+let c=4;
+c=5;
+c=6;
+document.write(c);
+document.write("<br>");
+
+// let newN = "hello";
+// local variable /scop
+ /*function myFunction (){ 
+    let nam ="Aminul";
+    document.write("This is" +" "+nam);
+  }
+  myFunction ();
+  document.write(nam); */
+  //global variable
+ /* let num = 4+5;
+  function info(){ 
+      document.write("this is positive number" +" "+num+" " +"positive");
+  }
+  info(); */
---
+++ non-primitive-data-type/main.js
@@ -0,0 +1,30 @@
+// Object Literals
+/*let profile ={ 
+  name:"Badhon",
+  id:12321,
+  subject:"CSE"
+}
+document.write(profile.name +" "+profile.id+" "+profile.subject);
+console.log(profile.name); */
+
+//Arrays Literals
+
+let trainees =["Rajib", "Ali", "Rafi","Rabbni"];
+document.write(trainees[2]);
+console.log(trainees.join("*"));
+console.log(trainees.concat("Sharmony","Rukiya"));
+console.log(trainees.toString());
+let letter =["B", "D", "C","A","E","G","F"];
+console.log(letter.sort());
+console.log(letter.reverse());
+
+let number =[5,6,8,9,1,3,2]
+console.log(number.sort());
+console.log(number.reverse());
+
+
+let cars =[];
+cars ="Toyota";
+cars ="t"
+
+console.log(cars[0]);
